8cbda1f34c30c324217f725ce3d0802610aae1d6
tgalen <tgalen84@gmail.com>
2022-08-01 11:11:59 -0400

    access data from accountActiveQuests

--- dfk_quest.js
+++ dfk_quest.js
@@ -7,31 +7,56 @@ const rxWalletAddress = "0x9aB773A84E0ACbf2F793B1ec267465293208dfB7";
 const privateKey = fs.readFileSync(".secret").toString().trim();
 const wallet = new ethers.Wallet(privateKey, provider);
 
-const heroCoreAddress = '0xEb9B61B145D6489Be575D3603F4a704810e143dF';
-const questCoreV2Address = '0xE9AbfBC143d7cef74b5b793ec5907fa62ca53154';
-const questCoreV2Abi = fs
-  .readFileSync("./abis/QuestCoreV2.json")
-  .toString();
+const heroCoreAddress = "0xEb9B61B145D6489Be575D3603F4a704810e143dF";
+const questCoreV2Address = "0xE9AbfBC143d7cef74b5b793ec5907fa62ca53154";
+const questCoreV2Abi = fs.readFileSync("./abis/QuestCoreV2.json").toString();
 const questCoreV2Contract = new ethers.Contract(
-    questCoreV2Address,
-    questCoreV2Abi,
-    provider
+  questCoreV2Address,
+  questCoreV2Abi,
+  provider
 );
 
-
 const quester = async () => {
-    console.log('-'.repeat(50));
-    console.log('\nHero 93141 Current Stamina:');
-    heroCurrentStamina = await questCoreV2Contract.functions.getCurrentStamina(93141);
-    console.log(heroCurrentStamina);
+  console.log("-".repeat(50));
+  console.log("\nHero 93141 Current Stamina:");
+  heroCurrentStamina = await questCoreV2Contract.functions.getCurrentStamina(
+    93141
+  );
+  console.log(heroCurrentStamina);
 
+  console.log("-".repeat(50));
+  console.log("\nWallet", rxWalletAddress, "Current Quests:");
+  accountActiveQuests =
+    await questCoreV2Contract.functions.getAccountActiveQuests(rxWalletAddress);
+  console.log("accountActiveQuests: " + typeof accountActiveQuests);
+  console.log("accountActiveQuests Keys: " + Object.keys(accountActiveQuests));
+  console.log("accountActiveQuests[0]: " + typeof accountActiveQuests[0]);
 
-    console.log('-'.repeat(50));
-    console.log('\nWallet', rxWalletAddress, 'Current Quests:');
-    accountActiveQuests = await questCoreV2Contract.functions.getAccountActiveQuests(rxWalletAddress);
+  console.log("Keys Length: " + Object.keys(accountActiveQuests).length);
+
+  const data = accountActiveQuests[0];
+  // const [id, questAddress, level, heroes, player, startBlock] = element;
+
+  console.log(`Data Length: ${data.length}`);
+  console.log(`Data: ${data}`);
+
+  data.forEach((element, i) => {
+    const [
+      id,
+      questAddress,
+      level,
+      heroes,
+      player,
+      startBlock,
+      startAtTime,
+      completeAtTime,
+      attempts,
+      status,
+    ] = element;
     console.log(
-        accountActiveQuests[0]
-        );
-}
+      `ID: ${id} || Quest Address: ${questAddress} || Level: ${level} || Hero: ${heroes} || Player: ${player} || Start Block: ${startBlock} || Start Time: ${startAtTime} || Complete Time: ${completeAtTime} || Attempts: ${attempts} || Status : ${status}`
+    );
+  });
+};
 
 quester();
