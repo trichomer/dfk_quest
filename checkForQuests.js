961131b2e7124976470acdebea3bf8cb7757d005
tgalen <tgalen84@gmail.com>
2022-08-08 17:20:07 -0400

    function completeQuest function

--- checkForQuests.js
+++ checkForQuests.js
@@ -1,4 +1,8 @@
 const { ethers } = require("ethers");
+const fs = require("fs");
+
+const privateKey = fs.readFileSync(".secret").toString().trim();
+
 const url = "https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc";
 
 let questContractDFKQCV2 = "0xE9AbfBC143d7cef74b5b793ec5907fa62ca53154";
@@ -12,14 +16,15 @@ const abi = [
   "event QuestReward(uint256 indexed questId, address indexed player, uint256 heroId, address rewardItem, uint256 itemQuantity)",
   "function multiStartQuest(address[] _questAddress, uint256[][] _heroIds, uint8[] _attempts, uint8[] _level) external",
 ];
+let provider = new ethers.providers.JsonRpcProvider(url);
+let questContract = new ethers.Contract(questContractDFKQCV2, abi, provider);
 
-let questContract, provider, wallet;
+const wallet = new ethers.Wallet(privateKey, provider);
+const callOptions = { gasPrice: 1600000000, gasLimit: 1000000 };
+const testWallet = "0x2E314D94fd218fA08A71bC6c9113e1b603B9d483";
 
 const main = async () => {
   try {
-    provider = new ethers.providers.JsonRpcProvider(url);
-    questContract = new ethers.Contract(questContractDFKQCV2, abi, provider);
-    wallet = "0x2E314D94fd218fA08A71bC6c9113e1b603B9d483";
     checkForQuests();
   } catch (err) {
     console.log(`${err.message}`);
@@ -29,7 +34,7 @@ const main = async () => {
 const checkForQuests = async () => {
   try {
     console.log("\nChecking for quests...\n");
-    let activeQuests = await questContract.getAccountActiveQuests(wallet);
+    let activeQuests = await questContract.getAccountActiveQuests(testWallet);
     console.log(`Active Quests: ${activeQuests}`);
     console.log(`Complete at Time : ${activeQuests[0][7]}`);
     console.log(`Current Time: ${Math.round(Date.now() / 1000)} `);
@@ -50,16 +55,74 @@ const checkForQuests = async () => {
 
     console.log(doneQuests.length + " done quests.");
 
-    doneQuests.forEach((quest) => {
+    for (quest of doneQuests) {
       console.log(`Hero ${quest.heroes[0]} is done questing.`);
-    });
-
-    // for (quest of doneQuests) {
-    //   console.log(`Hero ${quest.heroe[0]} is done questing.`);
-    // }
+    }
   } catch (err) {
-    console.log(err.message);
+    console.log("Check For Quests error: " + err.message);
   }
 };
 
+const completeQuest = async (heroId) => {
+  try {
+    console.log(`Completing quest led by hero ${heroId}.`);
+    let receipt = await tryTransaction(() => {
+      questContract.connect(wallet).completeQuest(heroId, callOptions);
+    }, 2);
+
+    console.log(`\n **** Completed quest led by hero ${heroId}. ****`);
+
+    let xpEvents = receipt.events.filter((e) => e.event === "QuestXP");
+    console.log(
+      `XP: ${xpEvents.reduce(
+        (total, result) => total + Number(result.args.xpEarned),
+        0
+      )}`
+    );
+
+    let suEvents = receipt.events.filter((e) => e.event === "QuestSkillUp");
+    console.log(
+      `SkillUp: ${
+        suEvents.reduce(
+          (total, result) => total + Number(result.args.skillUp),
+          0
+        ) / 10
+      }`
+    );
+
+    let rwEvents = receipt.events.filter((e) => e.event === "QuestReward");
+    rwEvents.forEach((result) =>
+      console.log(
+        `${result.args.itemQuantity} x ${getRewardDescription(
+          result.args.rewardItem
+        )}`
+      )
+    );
+
+    console.log("\n*****\n");
+  } catch (err) {
+    console.log("Complete Quest error: " + err.message);
+  }
+};
+
+const tryTransaction = async (transaction, attempts) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      var tx = await transaction();
+      let receipt = await tx.wait();
+      if (receipt.status !== 1)
+        throw new Error(`Receipt had a status of ${receipt.status}`);
+      return receipt;
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+    }
+  }
+};
+
+function getRewardDescription(rewardAddress) {
+  let desc = rewardLookup[rewardAddress];
+  return desc ? desc : rewardAddress;
+}
+
 main();
+completeQuest(282744);
