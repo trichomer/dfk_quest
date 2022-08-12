49acf6939f9768d97444ea756aff519f56485e3c
Big Al <alex.hamlett0@gmail.com>
2022-08-12 09:03:37 -0600

    new function to run startMeditation() and then completeMeditation() functions after a 40s wait

--- meditation.js
+++ meditation.js
@@ -8,27 +8,18 @@ const abi = fs.readFileSync("./abis/MeditationCircle.json").toString();
 const privateKey = fs.readFileSync(".secret").toString().trim();
 const wallet = new ethers.Wallet(privateKey, provider);
 const callOptions = { gasPrice: config.gasPrice, gasLimit: config.gasLimit };
-// const meditationContract = new ethers.Contract(
-//     meditationContractAddress,
-//     meditationAbi,
-//     wallet
-// );
-
-// heroId = 229837
 
 const start = async () => {
     try {
-        // let startMed = await meditationContract.startMeditation(
-        //     229837,
-        //     6,
-        //     4,
-        //     8,
-        //     null,
-        //     callOptions
-        // );
-
         let contract = new ethers.Contract(meditationContractAddress, abi, provider);
-        contract.connect(wallet).startMeditation(229837, 3, 2, 6, "0x0000000000000000000000000000000000000000", callOptions);
+        contract.connect(wallet).startMeditation(
+         229837, 
+         config.meditationStats, 
+         2, 
+         6, 
+         "0x0000000000000000000000000000000000000000", 
+         callOptions
+        );
 
         console.log(`Starting Meditation... ${contract}\n ...`);
 
@@ -48,5 +39,13 @@ const finish = async () => {
       }
 };
 
+const startAndFinish = () => {
+    start();
+    setTimeout(() => {
+     finish();
+    }, 40000);
+};
+
 // start();
-finish();
+// finish();
+startAndFinish();
