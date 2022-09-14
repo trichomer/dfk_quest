921e044042cfb4eb54398a499e48f10115159f06
Big Al <alex.hamlett0@gmail.com>
2022-09-13 21:25:07 -0600

    further listen API query testing

--- listen.js
+++ listen.js
@@ -15,105 +15,80 @@ const heroABI = [
 ];
 const config = require("./config.json");
 const https = require('https');
+const fetch = require('node-fetch');
 
-// Matches "/echo [whatever]"
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//     // 'msg' is the received Message from Telegram
-//     // 'match' is the result of executing the regexp above on the text content
-//     // of the message
-//     const chatId = msg.chat.id;
-//     const resp = match[1]; // the captured "whatever"
-//     // send back the matched "whatever" to the chat
-//     bot.sendMessage(chatId, resp);
-// });
-  // Listen for any kind of message. There are different kinds of
-  // messages.
-// bot.on('message', (msg) => {
-//     const chatId = msg.chat.id;
-//     // send a message to the chat acknowledging receipt of their message
-//     bot.sendMessage(chatId, 'Received your message, ' + msg.from.first_name);
-// });
-
-
-
-
-
-// let crystalContract = new ethers.Contract(
-//     crystalAddress,
-//     erc20Abi,
-//     provider
-// );
 let saleContract = new ethers.Contract(saleAddress, saleAbi, provider);
 
-// crystalContract.on("Transfer", (from, to, amount, event) => {
-//     console.log(`${ from } sent ${ ethers.utils.formatEther(amount) } to ${ to}`);
-// });
-
-// address = "0x7B8ee8e1127de4faa326726c9595FB8c2b29Ec42";
-// filter = crystalContract.filters.Transfer(null, address)
-// crystalContract.on(filter, (from, to, amount, event) => {
-//     console.log(`Received ${ ethers.utils.formatEther(amount) } from ${ from }`);
-// });
-
-// event AuctionCreated(
-// uint256 auctionId,
-// address indexed owner,
-// uint256 indexed tokenId,
-// uint256 startingPrice,
-// uint256 endingPrice,
-// uint256 duration,
-// address winner);
-
-// const getHeroDetails = async (heroId) => {
-//   try {
-//     let heroIdInt = Number(heroId);
-//     let heroDetails = gql########;
-
-//     console.log(heroDetails);
-//     console.log(heroDetails.length);
-//     // return heroDetails;
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// };
-
-
-
-const data = JSON.stringify({
-  query: ``,
-});
-
-const options = {
-  hostname: 'defi-kingdoms-community-api-gateway-co06z8vi.uc.gateway.dev',
-  path: '/graphql',
-  port: 443,
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Content-Length': data.length,
-    'User-Agent': 'Node',
-  },
-};
-
-const req = https.request(options, (res) => {
-  let data = '';
-  console.log(`statusCode: ${res.statusCode}`);
-
-  res.on('data', (d) => {
-    data += d;
+async function getData(id) {
+  const data = JSON.stringify({
+    query: `query myHeroes($id: ID!) {
+      heroes(where: {id: $id}, orderBy: rarity, orderDirection: desc){
+        id
+        level
+        generation
+        rarity
+        mainClass
+        subClass
+        summonsRemaining
+        maxSummons
+        hp
+        mp
+        strength
+        agility
+        endurance
+        wisdom
+        dexterity
+        vitality
+        intelligence
+        luck
+        statBoost1
+        statBoost2
+        profession
+        mining
+        fishing
+        gardening
+        foraging
+        statGenes
+        pjStatus
+        active1
+        active2
+        passive1
+        passive2
+        element
+        background
+        strengthGrowthP
+        agilityGrowthP
+        enduranceGrowthP
+        wisdomGrowthP
+        dexterityGrowthP
+        vitalityGrowthP
+        intelligenceGrowthP
+        luckGrowthP
+      }
+    }`,
+    variables: `{
+        "id": "${id}"
+      }`,
   });
-  res.on('end', () => {
-    console.log(JSON.parse(data).data);
-  });
-});
 
-req.on('error', (error) => {
-  console.error(error);
-});
+  const response = await fetch(
+    'https://defi-kingdoms-community-api-gateway-co06z8vi.uc.gateway.dev/graphql',
+    {
+      method: 'post',
+      body: data,
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+        'User-Agent': 'Node',
+      },
+    }
+  );
 
-req.write(data);
-req.end();
+  const json = await response.json();
+  console.log(json.data);
+}
 
+getData(93141);
 
 
 
@@ -130,7 +105,7 @@ saleContract.on(
         18
       )} CRYSTAL`
     );
-    // getHeroDetails(tokenId);
+    // let heroId = await getData(tokenId);
     bot.sendMessage(503468588, `Hero ${tokenId} posted for ${ethers.utils.formatUnits(startingPrice, 18)} CRYSTAL \n https://heroes.defikingdoms.com/image/${tokenId}`);
   }
 );
