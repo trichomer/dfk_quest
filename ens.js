d44f57d3ba12bcee396c996031e99fce9471009b
Big Al <alex.hamlett0@gmail.com>
2022-09-16 20:11:13 -0600

    further ENS

--- ens.js
+++ ens.js
@@ -11,10 +11,13 @@ const ensTest = async () => {
     const address = await provider.resolveName("onzozo.eth");
     const balance = await provider.getBalance("onzozo.eth");
     const lookup = await provider.lookupAddress("0x7B8ee8e1127de4faa326726c9595FB8c2b29Ec42");
+    const hijackPLBal = await parallelContract.balanceOf(address, 10824);
+    const hijackPLURI = await parallelContract.uri(10824);
 
     console.log(`Balance of ${address} is:`, ethers.utils.formatEther(balance), `ETH`);
     console.log(`Wallet ENS is: ${lookup}`);
-    
+    console.log(`Number of Hijack [PL] owned: ${hijackPLBal}`);
+    console.log(`Hijack[PL] Token URI: ${hijackPLURI}`);
 }
 
 ensTest();
