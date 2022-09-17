d54d767313ddbe4b24f6da15a7e0524bcdaa987c
Big Al <alex.hamlett0@gmail.com>
2022-09-16 20:05:51 -0600

    messing with ENS, ens.js

---
+++ ens.js
@@ -0,0 +1,20 @@
+const { ethers } = require("ethers");
+const fs = require("fs");
+const url = "https://eth-mainnet.g.alchemy.com/v2/Me9TM57oKMh4sMHIiLBSLgOfWG1SCqx0";
+const provider = new ethers.providers.JsonRpcProvider(url);
+const parallelAddress = "0x76BE3b62873462d2142405439777e971754E8E77";
+const parallelABI = fs.readFileSync("./abis/ParallelNFT.json").toString();
+const parallelContract = new ethers.Contract(parallelAddress, parallelABI, provider);
+
+
+const ensTest = async () => {
+    const address = await provider.resolveName("onzozo.eth");
+    const balance = await provider.getBalance("onzozo.eth");
+    const lookup = await provider.lookupAddress("0x7B8ee8e1127de4faa326726c9595FB8c2b29Ec42");
+
+    console.log(`Balance of ${address} is:`, ethers.utils.formatEther(balance), `ETH`);
+    console.log(`Wallet ENS is: ${lookup}`);
+    
+}
+
+ensTest();
