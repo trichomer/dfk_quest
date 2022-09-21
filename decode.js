bed3d8350ab75972cc1521afe57e697851622361
Big Al <alex.hamlett0@gmail.com>
2022-09-21 07:56:42 -0600

    starting statGene decoding; decode.js

--- decode.js
+++ decode.js
@@ -6,6 +6,24 @@ const provider = new ethers.providers.JsonRpcProvider(url);
 const heroABI = fs.readFileSync("./abis/HeroCore.json").toString();
 const heroContract = new ethers.Contract(DFKHeroCoreAddress, heroABI, provider);
 
+const STAT_GENE_MAP = {
+  0: 'class',
+  1: 'subClass',
+  2: 'profession',
+  3: 'passive1',
+  4: 'passive2',
+  5: 'active1',
+  6: 'active2',
+  7: 'statBoost1',
+  8: 'statBoost2',
+  9: 'statsUnknown1',
+  10: 'element',
+  11: 'statsUnknown2',
+};
+
+
+
+
 // const getHeroTest = async (id) => {
 //     let heroTest = await heroContract.getHero(id);
 //     let tupleTest = heroTest.map((item) => {item});
@@ -20,11 +38,11 @@ const heroContract = new ethers.Contract(DFKHeroCoreAddress, heroABI, provider);
 // getHeroTest(93141);
 
 
-heroContract.on(
-    "HeroUpdated",
-    (owner, tokenId, heroId, state) => {
-      console.log(
-        `test \n ${heroId} \n ${owner} \n ${tokenId}`
-      );
-    }
-  );
+// heroContract.on(
+//     "HeroUpdated",
+//     (owner, tokenId, heroId, state) => {
+//       console.log(
+//         `test \n ${heroId} \n ${owner} \n ${tokenId}`
+//       );
+//     }
+//   );
