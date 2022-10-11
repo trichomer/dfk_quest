e454560d492f8c9ec1e5d2508dd45ce648c89076
Big Al <alex.hamlett0@gmail.com>
2022-10-11 14:02:09 -0600

    comment

--- duel_score.js
+++ duel_score.js
@@ -25,24 +25,24 @@ const RARITY_ICON = {
 };
 
 // Fetch profile score
-const playerScore = async (prof, type) => {
-    let playerScore = await dfkDuelContract.getPlayerScore(prof, type);
-    console.log(`${prof} score for type ${type}: ${playerScore}`);
-};
-playerScore(config.queryWallet, 2);
+// const playerScore = async (prof, type) => {
+//     let playerScore = await dfkDuelContract.getPlayerScore(prof, type);
+//     console.log(`${prof} score for type ${type}: ${playerScore}`);
+// };
+// playerScore(config.queryWallet, 2);
 
 // Fetch individual hero scores from config
-const getHeroScore = async (id1, id2, id3, id4) => {
-    let heroScore1 = await dfkDuelContract.getCurrentHeroScoreDuelId(id1);
-    console.log(`Hero ${config.testHero1} Score: ${heroScore1}`);
-    let heroScore2 = await dfkDuelContract.getCurrentHeroScoreDuelId(id2);
-    console.log(`Hero ${config.testHero2} Score: ${heroScore2}`);
-    let heroScore3 = await dfkDuelContract.getCurrentHeroScoreDuelId(id3);
-    console.log(`Hero ${config.testHero3} Score: ${heroScore3}`);
-    let heroScore4 = await dfkDuelContract.getCurrentHeroScoreDuelId(id4);
-    console.log(`Hero ${config.testHero4} Score: ${heroScore4}`);
-  };
-getHeroScore(config.testHero1, config.testHero2, config.testHero3, config.testHero4);
+// const getHeroScore = async (id1, id2, id3, id4) => {
+//     let heroScore1 = await dfkDuelContract.getCurrentHeroScoreDuelId(id1);
+//     console.log(`Hero ${config.testHero1} Score: ${heroScore1}`);
+//     let heroScore2 = await dfkDuelContract.getCurrentHeroScoreDuelId(id2);
+//     console.log(`Hero ${config.testHero2} Score: ${heroScore2}`);
+//     let heroScore3 = await dfkDuelContract.getCurrentHeroScoreDuelId(id3);
+//     console.log(`Hero ${config.testHero3} Score: ${heroScore3}`);
+//     let heroScore4 = await dfkDuelContract.getCurrentHeroScoreDuelId(id4);
+//     console.log(`Hero ${config.testHero4} Score: ${heroScore4}`);
+//   };
+// getHeroScore(config.testHero1, config.testHero2, config.testHero3, config.testHero4);
 
 
 // Check wallet and return list of heroes
