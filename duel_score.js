db43fa56762a8952d28d7d598aeca8358c6b07c1
Big Al <alex.hamlett0@gmail.com>
2022-10-11 18:27:21 -0600

    install discord.js and dotenv

--- duel_score.js
+++ duel_score.js
@@ -10,7 +10,14 @@ const provider = new ethers.providers.JsonRpcProvider(url);
 const dfkDuelContract = new ethers.Contract(dfkDuelAddress, dfkDuelABI, provider);
 const dfkHeroContract = new ethers.Contract(dfkHeroAddress, dfkHeroABI, provider);
 const fetch = require('node-fetch');
-
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const commands = [
+    {
+      name: 'ping',
+      description: 'Replies with Pong!',
+    },
+  ];
 const COMMON = "⬜";
 const UNCOMMON = "🟩";
 const RARE = "🟦";
@@ -23,6 +30,37 @@ const RARITY_ICON = {
   1: UNCOMMON,
   0: COMMON,
 };
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+// Register slash commands with Discord API
+(async () => {
+    try {
+      console.log('Started refreshing application (/) commands.');
+  
+      await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+  
+      console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+      console.error(error);
+    }
+  })();
+  
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+  
+client.on('ready', () => {
+    console.log(`\n***Logged in as ${client.user.tag}***\n`);
+});
+  
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+    }
+});
+  
+client.login(process.env.TOKEN);
 
 // Check wallet from config
 const getHeroes = async (wallet) => {
