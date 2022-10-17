54ae0d4b7b2e335b183cb800351d1706b235ebbb
Big Al <alex.hamlett0@gmail.com>
2022-10-17 16:03:05 -0600

    further raffle.js

--- raffle.js
+++ raffle.js
@@ -1,11 +1,21 @@
 const { ethers } = require("ethers");
 const fs = require("fs");
 const config = require("./config.json");
+const dfkProvider = new ethers.providers.JsonRpcProvider(config.dfkRPC);
+const hmyProvider = new ethers.providers.JsonRpcProvider(config.hmyRPC);
+const profilesAddress = "0x6391F796D56201D279a42fD3141aDa7e26A3B4A5";
 const dfkRafAddress = "0xd8D7CE8921490b75EC489bd076AD0f27DC765675";
+const profilesABI = require("./abis/Profiles.json");
 const dfkRafABI = require("./abis/raffleMaster.json");
-const url = "https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc";
-const provider = new ethers.providers.JsonRpcProvider(url);
-const dfkRafContract = new ethers.Contract(dfkRafAddress, dfkRafABI, provider);
+const profilesContract = new ethers.Contract(profilesAddress, profilesABI, hmyProvider);
+const dfkRafContract = new ethers.Contract(dfkRafAddress, dfkRafABI, dfkProvider);
+const profile = async (owner) => {
+    let profName = await profilesContract.getProfileByAddress(owner);
+    console.log(`Profile name: ${profName[2]}`);
+    return profName;
+};
+
+
 
 // dfkRafContract.on(
 //     "RaffleEntered",
